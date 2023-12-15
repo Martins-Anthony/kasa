a43f0c084c8e921f1a5ed8ff0cd74cd7868e464a
Martins-Anthony <thony.martins.67@gmail.com>
2023-12-15 10:46:06 +0100

    card initialization

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,24 @@
+import React from 'react'
 import housingData from '../../datas/housing.json'
 import '../../scss/modules/card.scss'
 
 function Card() {
-  const itemList = housingData.map((item) => (
-    <div key={item.id} className="card-style">
-      <img src={item.cover} alt={item.title} />
-      <h2>{item.title}</h2>
-    </div>
-  ))
+  const itemList = housingData.map((item) => {
+    const spliteTitle = item.title.split(' - ')
+    const formatedTitle = spliteTitle.map((part, index) => (
+      <React.Fragment key={index}>
+        {part}
+        <br />
+      </React.Fragment>
+    ))
+
+    return (
+      <div key={item.id} className="card-style">
+        <img src={item.cover} alt={item.title} />
+        <h2>{formatedTitle}</h2>
+      </div>
+    )
+  })
 
   return <div className="card-container">{itemList}</div>
 }
