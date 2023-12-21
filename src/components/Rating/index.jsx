470a255589204ebc50c6468d3830f8c02087957f
Martins-Anthony <thony.martins.67@gmail.com>
2023-12-21 17:14:24 +0100

    media queries "host rating tags"

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,20 +1,31 @@
 import StartOn from '../../assets/profil/star-active-one.png'
 import StartOff from '../../assets/profil/star-active-two.png'
+import '../../scss/modules/rating.scss'
 
 function RatingStarts({ rating }) {
+  const range = [1, 2, 3, 4, 5]
   const countRating = parseInt(rating)
-  const maxRating = 5
-  const stars = []
-
-  for (let i = 1; i <= maxRating; i++) {
-    if (i <= countRating) {
-      stars.push(<img key={i} src={StartOn} alt="étoile" />)
-    } else {
-      stars.push(<img key={i} src={StartOff} alt="étoile" />)
-    }
-  }
-
-  return <div>{stars}</div>
+  return (
+    <div>
+      {range.map((rangeElem) =>
+        countRating >= rangeElem ? (
+          <img
+            className="star"
+            key={rangeElem.toString()}
+            src={StartOn}
+            alt="étoile"
+          />
+        ) : (
+          <img
+            className="star"
+            key={rangeElem.toString()}
+            src={StartOff}
+            alt="étoile"
+          />
+        ),
+      )}
+    </div>
+  )
 }
 
 export default RatingStarts
