b9e6153ed25bc4e5f49977b2d57888ad166089cd
Martins-Anthony <thony.martins.67@gmail.com>
2023-12-22 17:10:00 +0100

    media queries "slideshow"

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import ArrowLeft from '../../assets/slideshow/arrow_left.png'
-import ArrowRight from '../../assets/slideshow/arrow_right.png'
+import ArrowLeft from '../../assets/slideshow/arrow_left.svg'
+import ArrowRight from '../../assets/slideshow/arrow_right.svg'
 import '../../scss/modules/slideshow.scss'
 import React, { useState } from 'react'
 
 function Slideshow({ picture }) {
   let totalSlides = picture.length
+  let visibleArrowAndCounter = true
 
   const [currentSlide, setCurrentSlide] = useState(0)
+
+  if (totalSlides <= 1) {
+    visibleArrowAndCounter = false
+  }
 
   const goToNextSlide = () => {
     setCurrentSlide((precSlide) => (precSlide + 1) % totalSlides)
@@ -24,18 +29,26 @@ function Slideshow({ picture }) {
         alt="image logement"
       />
       <img
-        className="slideshow-arrow arrow-left"
+        className={`slideshow-arrow arrow-left ${
+          visibleArrowAndCounter ? '' : 'hide-arrow-and-counter'
+        }`}
         src={ArrowLeft}
         alt="Flèche gauche"
         onClick={goToPrevSlide}
       />
       <img
-        className="slideshow-arrow arrow-right"
+        className={`slideshow-arrow arrow-right ${
+          visibleArrowAndCounter ? '' : 'hide-arrow-and-counter'
+        }`}
         src={ArrowRight}
         alt="Flèche droite"
         onClick={goToNextSlide}
       />
-      <div className="slideshow-count">
+      <div
+        className={`slideshow-count ${
+          visibleArrowAndCounter ? '' : 'hide-arrow-and-counter'
+        }`}
+      >
         {currentSlide + 1} / {totalSlides}
       </div>
     </div>
